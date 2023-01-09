12fcb9ec225e04000dd3ff29ce358ed0b9f2863e
Mikołaj <mrefe12345@gmail.com>
2023-01-09 16:14:02 +0100

    init

---
+++ recipefinder/recipefinder/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using recipefinder.Models;
+using System.Diagnostics;
+using Azure.Storage.Blobs;
+
+namespace recipefinder.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration config;
+        //private readonly HttpContext hc;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration config) {
+            _logger = logger;
+            this.config = config;
+        }
+
+        [HttpPost("getrecipe")]
+        public async Task<IActionResult> DoSomething([FromForm]IFormFile image) {
+            /* Tutaj dodać łączenie z mlem i jego odpowiedź sparsować na recipeName */
+
+
+
+
+
+            string recipeName = "apple_pie"; // to co wypluje ML
+            var connectionString = config.GetValue<string>("blobConnectionString");
+            var blobContainerName = config.GetValue<string>("blobContainerName");
+            BlobClient blobClient = new BlobClient(connectionString, blobContainerName, recipeName);
+            using(var stream = new MemoryStream()) {
+                await blobClient.DownloadToAsync(stream);
+                stream.Position = 0;
+                var contentType = (await blobClient.GetPropertiesAsync()).Value.ContentType;
+                Console.WriteLine("JESTEM TU HURA");
+                return File(stream.ToArray(), contentType, blobClient.Name);
+            }
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ recipefinder/recipefinder/Views/Home/validation.js
@@ -0,0 +1,30 @@
+﻿const form = document.getElementById('form');
+const fileInput = document.getElementById('file-input');
+const preview = document.getElementById('preview');
+
+fileInput.addEventListener('change', event => {
+    // Get the selected file
+    const file = event.target.files[0];
+
+    // Create a URL for the file
+    const url = URL.createObjectURL(file);
+
+    // Set the src attribute of the image element
+    preview.src = url;
+});
+
+form.addEventListener('submit', async event => {
+    event.preventDefault();
+
+    // Create a new FormData object
+    const formData = new FormData();
+
+    // Append the file input element to the FormData object
+    formData.append('photo', fileInput.files[0]);
+
+    // Send the POST request to the API
+    fetch('https://recipefinder7.azurewebsites.net/getrecipe', {
+        method: 'POST',
+        body: formData
+    });
+});
