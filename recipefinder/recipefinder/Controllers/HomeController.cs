fde51788cd28808d92f2d82844f9ab1c2ea600c1
Mikołaj <mrefe12345@gmail.com>
2023-01-10 19:39:41 +0100

    Added mobile endpoints

--- recipefinder/recipefinder/Controllers/HomeController.cs
+++ recipefinder/recipefinder/Controllers/HomeController.cs
@@ -31,7 +31,6 @@ public class HomeController : Controller
         foreach (var file in uploadedFile)
         {
             string Filename = file.FileName;
-            Console.WriteLine(Filename);
         }
         var image = uploadedFile[0];
 
@@ -63,7 +62,6 @@ public class HomeController : Controller
         }
 
         string recipeName = jsonResponse.labels[maxIndex]; // to co wypluje ML
-        Console.WriteLine(recipeName);
         var connectionString = config.GetValue<string>("blobConnectionString");
         var blobContainerName = config.GetValue<string>("blobContainerName");
         BlobClient blobClient = new BlobClient(connectionString, blobContainerName, recipeName);
@@ -72,11 +70,88 @@ public class HomeController : Controller
             await blobClient.DownloadToAsync(stream);
             stream.Position = 0;
             var contentType = (await blobClient.GetPropertiesAsync()).Value.ContentType;
-            //Console.WriteLine("JESTEM TU HURA");
             return File(stream.ToArray(), contentType, blobClient.Name);
         }
     }
 
+
+    [HttpPost("getrecipeMobile")]
+    [AllowAnonymous]
+    public async Task<IActionResult> DoSomething2([FromBody] byte[] image) {
+
+        WebRequest request = HttpWebRequest.Create("http://5613cafb-fb2a-4132-9a3e-0d32dc030956.northeurope.azurecontainer.io/score");
+        request.Method = "POST";
+        request.Headers.Add("Content-Type", "application/octet-stream");
+        using (var ms = new MemoryStream(image)) {
+            var fileBytes = ms.ToArray();
+            request.ContentLength = fileBytes.Length;
+            Stream stream = request.GetRequestStream();
+            stream.Write(fileBytes, 0, fileBytes.Length);
+            stream.Close();
+        }
+        HttpWebResponse response = (HttpWebResponse)request.GetResponseAsync().Result;
+
+        string json = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+        ResponseJson? jsonResponse = JsonSerializer.Deserialize<ResponseJson>(json);
+        int maxIndex = 0;
+        for (int i = 0; i < jsonResponse.probs.Length; i++) {
+            if (jsonResponse.probs[i] > jsonResponse.probs[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        string recipeName = jsonResponse.labels[maxIndex]; // to co wypluje ML
+        var connectionString = config.GetValue<string>("blobConnectionString");
+        var blobContainerName = config.GetValue<string>("blobContainerName");
+        BlobClient blobClient = new BlobClient(connectionString, blobContainerName, recipeName);
+        using (var stream = new MemoryStream()) {
+            await blobClient.DownloadToAsync(stream);
+            stream.Position = 0;
+            var contentType = (await blobClient.GetPropertiesAsync()).Value.ContentType;
+            return File(stream.ToArray(), contentType, blobClient.Name);
+        }
+    }
+
+    [HttpPost("getrecipeMobileZwei")]
+    [AllowAnonymous]
+    public async Task<IActionResult> DoSomething3([FromBody] IFormFile image) {
+
+        WebRequest request = HttpWebRequest.Create("http://5613cafb-fb2a-4132-9a3e-0d32dc030956.northeurope.azurecontainer.io/score");
+        request.Method = "POST";
+        request.Headers.Add("Content-Type", "application/octet-stream");
+        var f = image.OpenReadStream();
+        using (var ms = new MemoryStream()) {
+            f.CopyTo(ms);
+            var fileBytes = ms.ToArray();
+            request.ContentLength = fileBytes.Length;
+            Stream stream = request.GetRequestStream();
+            stream.Write(fileBytes, 0, fileBytes.Length);
+            stream.Close();
+        }
+        HttpWebResponse response = (HttpWebResponse)request.GetResponseAsync().Result;
+
+        string json = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+        ResponseJson? jsonResponse = JsonSerializer.Deserialize<ResponseJson>(json);
+        int maxIndex = 0;
+        for (int i = 0; i < jsonResponse.probs.Length; i++) {
+            if (jsonResponse.probs[i] > jsonResponse.probs[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        string recipeName = jsonResponse.labels[maxIndex]; // to co wypluje ML
+        var connectionString = config.GetValue<string>("blobConnectionString");
+        var blobContainerName = config.GetValue<string>("blobContainerName");
+        BlobClient blobClient = new BlobClient(connectionString, blobContainerName, recipeName);
+        using (var stream = new MemoryStream()) {
+            await blobClient.DownloadToAsync(stream);
+            stream.Position = 0;
+            var contentType = (await blobClient.GetPropertiesAsync()).Value.ContentType;
+            return File(stream.ToArray(), contentType, blobClient.Name);
+        }
+    }
 
     [AllowAnonymous]
     public IActionResult Index()
